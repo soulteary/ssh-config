06f91c6b4fdda9bc11bb77177ea80eb7dfa0fa6c
Su Yang <soulteary@gmail.com>
2024-10-13 21:23:59 +0800

    chore: ssh config entry

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/soulteary/ssh-yaml
+
+go 1.23.0
+
+require gopkg.in/yaml.v2 v2.4.0
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	Define "github.com/soulteary/ssh-yaml/internal/define"
+	Fn "github.com/soulteary/ssh-yaml/internal/fn"
+	Parser "github.com/soulteary/ssh-yaml/internal/parser"
+)
+
+func CheckUseStdin() bool {
+	fi, err := os.Stdin.Stat()
+	if err != nil {
+		fmt.Println("Error getting stdin stat:", err)
+		return false
+	}
+	if (fi.Mode() & os.ModeCharDevice) == 0 {
+		return true
+	}
+	return false
+}
+
+func main() {
+	toYAML := flag.Bool("to-yaml", false, "Convert SSH config(Text/JSON) to YAML")
+	toSSH := flag.Bool("to-ssh", false, "Convert SSH config(YAML/JSON) to YAML")
+	toJSON := flag.Bool("to-json", false, "Convert SSH config(YAML/Text) to JSON")
+	doTest := flag.Bool("test", false, "Test the SSH config")
+	flag.Parse()
+
+	if (*toYAML == *toSSH && *toYAML == *toJSON) || (*toSSH == *toJSON && *toSSH == *toYAML) || (*toJSON == *toYAML && *toJSON == *toSSH) {
+		fmt.Println("Please specify either -to-yaml or -to-ssh or -to-json")
+		os.Exit(1)
+	}
+
+	if *doTest {
+		if CheckUseStdin() {
+			input := Fn.GetUserInputFromStdin()
+			fileType := Fn.DetectStringType(input)
+
+			switch strings.ToUpper(fileType) {
+			case "YAML":
+				fmt.Println("YAML")
+			case "JSON":
+				fmt.Println("JSON")
+			case "TEXT":
+				configs := Parser.GroupSSHConfig(input)
+				hostConfigs := make([]Define.HostConfig, 0)
+				for host, hostConfig := range configs {
+					rawInfo := Parser.GetSSHConfigContent(host, hostConfig)
+					hostInfo := Parser.ParseSSHConfig(rawInfo.Config, rawInfo.Comments)
+					config, name, notes := Parser.GetSingleHostData(hostInfo)
+
+					hostConfigs = append(hostConfigs, Define.HostConfig{
+						Name:   name,
+						Notes:  notes,
+						Config: config,
+					})
+				}
+
+				if *toYAML {
+					fmt.Println(string(Parser.ConvertToYAML(hostConfigs)))
+				}
+
+				if *toSSH {
+					fmt.Println(string(Parser.ConvertToSSH(hostConfigs)))
+				}
+
+				if *toJSON {
+					fmt.Println(string(Parser.ConvertToJSON(hostConfigs)))
+				}
+			}
+		} else {
+			fmt.Println("Use Files")
+		}
+		os.Exit(0)
+	}
+}
